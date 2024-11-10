fc3ad5515acc8470fa2e3e8eb48ec3bf3315b7e5
advillanuevag <vil24014@byui.edu>
2024-11-10 12:15:52 -0300

    Learning02 Assignment. Creates job1 and job2 objects. Create myResume List of objects Type Job and add job1 and job2 objects to its list. Finally calle DisplayResumeDetails() Method from Resume class.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -11,6 +11,38 @@ class Program
                    you might see in LinkedIn.
         */
 
-        
+        //Create a new Job instance called job1:
+        Job job1 = new Job();
+
+        //Assign values to job1 variables:
+        job1._companyName = "MicroSoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = "2022";
+        job1._endYear = "2023";
+
+        //Create a new Job instance called job2:
+        Job job2 = new Job();
+
+        //Assign values to job2 variables:
+        job2._companyName = "Apple";
+        job2._jobTitle = "Senior Software Engineer";
+        job2._startYear = "2022";
+        job2._endYear = "2024";
+
+        //Create a new Resume instance called myResume:
+        Resume myResume = new Resume();
+
+        //Assignes a value to _personName variable:
+        myResume._personName = "Alberto Villanueva";
+
+        //Adds objects job1 and job2 to myResume:
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        //Calls DisplayResumeDetails() Method from Resume class.
+        myResume.DisplayResumeDetails();
+
+
+
     }
 }
