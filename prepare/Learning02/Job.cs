a9681d464c6fc65bded47003ce57e7aa5aebbb15
advillanuevag <vil24014@byui.edu>
2024-11-10 10:54:42 -0300

    Learning02 Assignment. Create Job class and declare its variable names.

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,9 @@
+public class Job
+{
+    //Create the class Job and Declare class variable names:
+    public string _companyName;
+    public string _jobTitle;
+    public string _startYear;
+    public string _endYear;
+
+}
