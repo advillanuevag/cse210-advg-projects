7542fd15dc308234530a90b31c8efa21a1b86a79
advillanuevag <vil24014@byui.edu>
2024-11-10 11:40:01 -0300

    Learning02 Assignment. Declares DisplayResumeDetails() Method to show the Person´s name and his/her jobs from DisplayJobDetails() Method.

--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -7,4 +7,20 @@ public class Resume{
     //of Type Job.
 
     public List<Job> _jobs = new List<Job>();
+
+    //Declares DisplayResumeDetails() Method to show the Person´s Name and
+    //his or her Jobs:
+    public void DisplayResumeDetails()
+    {
+        Console.WriteLine($"name: {_personName}");
+        Console.WriteLine("Jobs:");
+
+        //Travers the List _jobs, and for each element in the list (pega)
+        //calls the DisplayJobDetails() Method using dot notation:
+        foreach(Job pega in _jobs)
+        {
+            pega.DisplayJobDetails();
+        }
+
+    }
 }   
