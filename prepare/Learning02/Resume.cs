d1cff16f5056a9e0ec366fab2b97bca5de0580ec
advillanuevag <vil24014@byui.edu>
2024-11-10 11:25:24 -0300

    Learning02 Assignment. Creates the Resume class, declares a variable named _personName and also declares and instantiate the Object _jobs as a List of Objetcs of Type Job.

---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,10 @@
+public class Resume{
+    //Creates Resume class and declares it´s variable _personName:
+
+    public string _personName;
+
+    //Declares and instantiate the object _jobs as a List of objects
+    //of Type Job.
+
+    public List<Job> _jobs = new List<Job>();
+}   
