587e1be3467b0a27a2df53b2b6c4a537e9e7d1ed
advillanuevag <vil24014@byui.edu>
2024-11-16 11:24:46 -0300

    Defines _entries as a List of Entry objects. Defines AddEntry() method to append an Entry into the List _entries.

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,3 +1,12 @@
-public class Journal{
-    
+public class Journal
+{
+    //Defines the variable _entries as a List of Entry objects:
+    public List<Entry> _entries = new List<Entry>();
+
+    //Defines the AddEntry() method to append an Entry into the List _entries:
+    public void AddEntry(Entry newEntry)
+    {
+        _entries.Add(newEntry);
+
+    }
 }
