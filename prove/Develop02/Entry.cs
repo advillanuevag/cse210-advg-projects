cb7e1aa740aa8b486cec48fd329d7b1545846956
advillanuevag <vil24014@byui.edu>
2024-11-16 11:10:30 -0300

    Declares and creation of Entry class and it member variables. Declares and definition of DisplayEntry() method to show entry details.

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,17 @@
+public class Entry
+{
+    //Declares class Entry variables:
+    public string _date;
+    public string _randomPrompt;
+    public string _userResponse;
+
+    //Declares and define DisplayEntry() method to show Entry information:
+    public void DisplayEntry()
+    {
+        Console.WriteLine("");
+        Console.WriteLine($"Date:v{_date} - Prompt: {_randomPrompt}");
+        Console.WriteLine($"{_userResponse}");
+        Console.WriteLine("");
+    }
+
+}
